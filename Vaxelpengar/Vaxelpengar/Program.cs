091920c3e94963dfb03ea355892113bf5f2b13bd
Mattias Wikström <mw222rs@student.lnu.se>
2014-09-09 20:51:06 +0200

    Försök att räkna växel

--- Vaxelpengar/Vaxelpengar/Program.cs
+++ Vaxelpengar/Vaxelpengar/Program.cs
@@ -13,8 +13,9 @@ namespace Vaxelpengar
             // Variablerna.
             double totalSumma;
             int kontanter;
-            uint  attBetala;
+            uint attBetala;
             double avrundningsTal;
+            int tillbaka;
 
             // Läs in totalsumman.
             while (true)
@@ -27,9 +28,12 @@ namespace Vaxelpengar
                     if (totalSumma < 1)
                     {
                         Console.WriteLine("Fel! Totalsumman är för liten, köpet kunde inte genomföras.");
-                        return;
+
                     }
-                    break;
+                    else
+                    {
+                        break;
+                    }
                 }
                 catch (FormatException)
                 {
@@ -45,15 +49,18 @@ namespace Vaxelpengar
                     Console.Write("Ange mottaget belopp     : ");
                     kontanter = int.Parse(Console.ReadLine());
 
-                    if (kontanter < totalSumma)
-                    {
-                        Console.WriteLine("Fel! Pengarna räcker inte.");
-                    }
-                    else if (kontanter < 1)
+                    if (kontanter < 1)
                     {
                         Console.WriteLine("Fel! Det går inte att betala med minus.");
                     }
-                    break;
+                    else if (kontanter < totalSumma)
+                    {
+                        Console.WriteLine("Fel! Pengarna räcker inte.");
+                    }                    
+                    else
+                    {
+                        break;
+                    }
                 }
                 catch (FormatException)
                 {
@@ -64,15 +71,33 @@ namespace Vaxelpengar
             // Avrunda
             attBetala = (uint)Math.Round(totalSumma);
             avrundningsTal = attBetala - totalSumma;
-
-            // Kvitto
+            tillbaka = kontanter - (int)attBetala;
+            
+            // Kvittoutskrift
             Console.WriteLine("|KVITTO----------------------");
             Console.WriteLine("|Totalt: {0, 16} kr", totalSumma);
             Console.WriteLine("|Öresavrundning: {0, 8:f2} kr", avrundningsTal);
             Console.WriteLine("|Att betala: {0, 12} kr", attBetala);
             Console.WriteLine("|Kontant: {0, 15} kr", kontanter);
-            Console.WriteLine("|Tillbaka: {0, 14} kr", (kontanter - attBetala));
+            Console.WriteLine("|Tillbaka: {0, 14} kr", (tillbaka));
             Console.WriteLine("-----------------------------");
+
+            // Sedelantal
+            for (; tillbaka < 500; tillbaka -= 500)
+            {
+                Console.WriteLine("FEmhundra"); 
+            }
+
+            //Console.WriteLine("500-lappar: {0}", ((kontanter - attBetala) / 500));
+            //Console.WriteLine("100-lappar: {0}", ((kontanter - attBetala) % 500) / 100);
+            //Console.WriteLine("50-lappar: {0}", ((kontanter - attBetala) / 500));
+            //Console.WriteLine("20-lappar: {0}", ((kontanter - attBetala) / 500));
+            //Console.WriteLine("10-kronor: {0}", ((kontanter - attBetala) / 500));
+            //Console.WriteLine("5-kronor: {0}", ((kontanter - attBetala) / 500));
+            //Console.WriteLine("1-kronor: {0}", ((kontanter - attBetala) / 500));
+
+            // Omstart
+
 
             
 
