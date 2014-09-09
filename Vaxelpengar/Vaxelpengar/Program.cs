e52b8e8fa0d7a9c015940375d0f117a06542293c
Mattias Wikström <mw222rs@student.lnu.se>
2014-09-09 13:59:27 +0200

    Börjat om från början

--- Vaxelpengar/Vaxelpengar/Program.cs
+++ Vaxelpengar/Vaxelpengar/Program.cs
@@ -10,28 +10,36 @@ namespace Vaxelpengar
     {
         static void Main(string[] args)
         {
-            // Ange variabler
-            double totalSumma;
-            int kontanter;
+            // Indata: Totalsumma och erhållet belopp. Totalsumma skall kunna anges i kronor och ören, erhållet belopp skall kunna anges i hela kronor.
 
-            // Läs in köpsumma.
-            Console.Write("Ange den totala köpsumman: ");
-            totalSumma = double.Parse(Console.ReadLine());
-            
-            if (totalSumma < 1)
-            {
-                Console.WriteLine("FEL! {0:c} är mindre än 1 kr.", totalSumma);
-            }
+            // Inläsning av totalsumma måste placeras i en separat statisk metod.
 
-            // Läs in kontantbelopp.
-            Console.Write("Mottaget kontantbelopp   : ");
-            kontanter = int.Parse(Console.ReadLine());
+            // Inmatad totalsumma avrundas till närmsta hela krontal.
 
+            // Inläsning av erhållet belopp måste placeras i en separat statisk metod. 
 
+            /* Metoden Main skall skriva ut kvitto på följande: 
+             * a) Köpets totala summa.
+             * b) Öresavrundningen.
+             * c) Summa att betala efter öresavrundningen
+             * d) Erhållet belopp (kontant).
+             * e) Växeln kunden skall ha tillbaka. */
 
-            // Visa kvitto.
+            /* Metoden SplitIntoDenominations skall dela upp, och skriva ut, växeln i lämpligt antal 
+             * 500-, 100, 50- och 20-lappar samt antal 10-, 5- och 1-kronor och bara presenteras om antalet
+             * sedlar och/eller mynt är större än 0.*/
 
-            // Visa sedlar och mynt.
+            /* Då antalet sedlar och mynt ska bestämmas måste följande uppfyllas:
+             * a) Kod för beräkning och presentation måste placeras i en separat statisk metod.
+             * b) Division- och modulusoperatorerna måste användas.
+             * c) En array innehållande giltiga valörer måste användas.
+             * d) Endast en ”if”-sats får användas. */
+
+            /* Eventuella fel i samband med inmatning av totalsumma och erhållet belopp ska tas om hand 
+             * med hjälp av ”try-catch”-satser och användaren ska få en möjlighet att mata in ett nytt 
+             * värde.
+             * Figurerna Figur B.6 till Figur B.9 visar exempel på några felaktiga inmatningar som måste 
+             * hanteras och ge användaren en ny möjlighet att mata in ett korrekt värde. */
 
         }
     }
