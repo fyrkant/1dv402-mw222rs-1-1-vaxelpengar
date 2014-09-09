cf862640118880d64943ae1627bff1e389f84218
Mattias Wikström <mw222rs@student.lnu.se>
2014-09-09 16:14:04 +0200

    Pillat med kvittoutskrift

--- Vaxelpengar/Vaxelpengar/Program.cs
+++ Vaxelpengar/Vaxelpengar/Program.cs
@@ -26,26 +26,55 @@ namespace Vaxelpengar
 
                     if (totalSumma < 1)
                     {
-                        Console.WriteLine("Fel! ");
+                        Console.WriteLine("Fel! Totalsumman är för liten, köpet kunde inte genomföras.");
                         return;
                     }
                     break;
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine();
+                    Console.WriteLine("Endast siffror, tack!");
                 } 
             }
 
             // Läs in kontantbelopp
-            Console.Write("Ange mottaget belopp: ");
-            kontanter = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Ange mottaget belopp     : ");
+                    kontanter = int.Parse(Console.ReadLine());
 
+                    if (kontanter < totalSumma)
+                    {
+                        Console.WriteLine("Fel! Pengarna räcker inte.");
+                    }
+                    else if (kontanter < 1)
+                    {
+                        Console.WriteLine("Fel! Det går inte att betala med minus.");
+                    }
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Endast siffror, tack!");
+                } 
+            }
+
+            // Avrunda
             attBetala = (uint)Math.Round(totalSumma);
             avrundningsTal = attBetala - totalSumma;
 
-            // Skriv ut ett kvitto.
-            Console.WriteLine("Ge kunden tillbaka {o:c}.", (kontanter - totalSumma));
+            // Kvitto
+            Console.WriteLine("|KVITTO----------------------");
+            Console.WriteLine("|Totalt: {0, 16} kr", totalSumma);
+            Console.WriteLine("|Öresavrundning: {0, 8:f2} kr", avrundningsTal);
+            Console.WriteLine("|Att betala: {0, 12} kr", attBetala);
+            Console.WriteLine("|Kontant: {0, 15} kr", kontanter);
+            Console.WriteLine("|Tillbaka: {0, 14} kr", (kontanter - attBetala));
+            Console.WriteLine("-----------------------------");
+
+            
 
 
         }
