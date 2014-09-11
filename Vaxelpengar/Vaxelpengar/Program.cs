0eb184efb4ab5f3e0a8f7acb63c9af57f6492f35
Mattias Wikström <mw222rs@student.lnu.se>
2014-09-11 08:27:49 +0200

    småfixande med utseendet på utskrift

--- Vaxelpengar/Vaxelpengar/Program.cs
+++ Vaxelpengar/Vaxelpengar/Program.cs
@@ -27,7 +27,7 @@ namespace Vaxelpengar
 
                     if (totalSumma < 1)
                     {
-                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
                         Console.WriteLine("Fel! Totalsumman är för liten, köpet kunde inte genomföras.");
                         Console.ResetColor();
                     }
@@ -38,8 +38,8 @@ namespace Vaxelpengar
                 }
                 catch (FormatException)
                 {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Endast siffror, tack!");
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Otillåten inmatning, försök igen.");
                     Console.ResetColor();
                 } 
             }
@@ -54,13 +54,13 @@ namespace Vaxelpengar
 
                     if (kontanter < 1)
                     {
-                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
                         Console.WriteLine("Fel! Det går inte att betala med minus.");
                         Console.ResetColor();
                     }
                     else if (kontanter < totalSumma)
                     {
-                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
                         Console.WriteLine("Fel! Pengarna räcker inte.");
                         Console.ResetColor();
                     }                    
@@ -71,8 +71,8 @@ namespace Vaxelpengar
                 }
                 catch (FormatException)
                 {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Endast siffror, tack!");
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Otillåten inmatning, försök igen.");
                     Console.ResetColor();
                 } 
             }
@@ -83,58 +83,62 @@ namespace Vaxelpengar
             tillbaka = kontanter - (int)attBetala;
             
             // Kvittoutskrift
+            Console.WriteLine();
             Console.WriteLine(" KVITTO                      ");
-            Console.WriteLine("|----------------------------");
-            Console.WriteLine("|Totalt        : {0, 8} kr|", totalSumma);
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(" Totalt        : {0, 8} kr", totalSumma);
 
             // Visa inte öresavrundningen om ingen avrundning skett.
             if (avrundningsTal != 0)
             {
-            Console.WriteLine("|Öresavrundning: {0, 8:f2} kr|", avrundningsTal); 
+            Console.WriteLine(" Öresavrundning: {0, 8:f2} kr", avrundningsTal); 
             }
 
-            Console.WriteLine("|Att betala    : {0, 8} kr|", attBetala);
-            Console.WriteLine("|Kontant       : {0, 8} kr|", kontanter);
-            Console.WriteLine("|Tillbaka      : {0, 8} kr|", (tillbaka));
+            Console.WriteLine(" Att betala    : {0, 8} kr", attBetala);
+            Console.WriteLine(" Kontant       : {0, 8} kr", kontanter);
+            Console.WriteLine(" Tillbaka      : {0, 8} kr", (tillbaka));
             Console.WriteLine("-----------------------------");
+            Console.WriteLine();
 
             // Sedelantal
             if (tillbaka >= 500)
             {
-                Console.WriteLine("500-lappar: {0, 15}", (tillbaka / 500));
+                Console.WriteLine("500-lappar     : {0}", (tillbaka / 500));
                 tillbaka %= 500; 
             }
             if (tillbaka >= 100)
             {
-                Console.WriteLine("100-lappar: {0, 15}", (tillbaka / 100));
+                Console.WriteLine("100-lappar     : {0}", (tillbaka / 100));
                 tillbaka %= 100; 
             }
             if (tillbaka >= 50)
             {
-                Console.WriteLine(" 50-lappar: {0, 15}", (tillbaka / 50));
+                Console.WriteLine(" 50-lappar     : {0}", (tillbaka / 50));
                 tillbaka %= 50; 
             }
             if (tillbaka >= 20)
             {
-                Console.WriteLine(" 20-lappar: {0, 15}", (tillbaka / 20));
+                Console.WriteLine(" 20-lappar     : {0}", (tillbaka / 20));
                 tillbaka %= 20; 
             }
             if (tillbaka >= 10)
             {
-                Console.WriteLine(" 10-kronor: {0, 15}", (tillbaka / 10));
+                Console.WriteLine(" 10-kronor     : {0}", (tillbaka / 10));
                 tillbaka %= 10; 
             }
             if (tillbaka >= 5)
             {
-                Console.WriteLine("  5-kronor: {0, 15}", (tillbaka / 5));
+                Console.WriteLine("  5-kronor     : {0}", (tillbaka / 5));
                 tillbaka %= 5; 
             }
             if (tillbaka >= 1)
             {
-                Console.WriteLine("  1-kronor: {0, 15}", tillbaka); 
+                Console.WriteLine("  1-kronor     : {0}", tillbaka); 
             }
           
             // Avslut.
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("Välkommen åter! Valfri knapp avslutar programmet.");
             Console.ReadKey();
 
